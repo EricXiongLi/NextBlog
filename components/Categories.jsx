4fd6e3ae5e0d4c91f79bf46abed0baa01eaaca5b
EricXiongLi <xiong.li@uzh.ch>
2022-04-16 21:14:54 +0200

    implemented all functionalities

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { getCategories } from "../services";
 
 function Categories() {
-  return <div>Categories</div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pd-4">Categories</h3>
+      {categories.map((category) => (
+        <Link key={category.slug} href={`/category/${category.slug}`}>
+          <span className="cursor-pointer block pb-3 mb-3">
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Categories;
